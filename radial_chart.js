54562cf9f141197af3a3bd880f0f6ccdc7c12ac3
Shannel <jpor0014@student.monash.edu>
2025-10-17 17:33:36 +0800

    Update radial_chart.js

--- radial_chart.js
+++ radial_chart.js
@@ -40,7 +40,7 @@ const radialFnbSpec = {
       },
       sort: ["1-10","11-50","51-200","201-500","501-1,000","1,001-5,000","5,001-10,000"],
       legend: {
-        orient: "top-right",
+        orient: "top-left",
         direction: "vertical",
         columns: 1,
         titleFontSize: 12,
